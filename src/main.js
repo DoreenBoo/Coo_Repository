aa5dd6be65e4d28a0a8339cee98e78791302851a
Lucas <2870533409@qq.com>
2024-11-12 08:46:48 +0800

    haha

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-//引入element plus 组件
+
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 app.use(ElementPlus);
 app.mount('#app');
+
+
+createApp(App).mount('#app')
